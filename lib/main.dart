7cec480acba5cd433f01f848a61f6cf0576118b1
muhammadkamrannazir <94740513+muhammadkamrannazir@users.noreply.github.com>
2022-10-22 14:04:12 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'memo.dart';
+
+Future<void> main() async {
+  // Initializing Firestore
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBlm_EQi7cs3Z3frfqUfg3F8hqnqVU2kFU",
+      authDomain: "fluttermemoapplication.firebaseapp.com",
+      projectId: "fluttermemoapplication",
+      storageBucket: "fluttermemoapplication.appspot.com",
+      messagingSenderId: "116888741394",
+      appId: "1:116888741394:web:d0baf78f4e5be239769acb",
+    ),
+  );
+  runApp(const Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  // Home Widget
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Memo Pro',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const Memo(),
+    );
+  }
+}
